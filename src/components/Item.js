4444ebb330e8b189e34a7fab136e2a55329b3f3b
Melissa Wang <melissa.wang21@gmail.com>
2024-09-17 12:38:28 -0400

    state and events lab complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  let [inCart, setInCart] = useState(false)
+
+  function handleClick() {
+    setInCart((inCart) => !inCart)
+  }
+
   return (
-    <li className="">
+    <li className={ inCart ? "in-cart" : "" }>
       <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <span className="category">{ category }</span>
+      <button 
+        className={ inCart ? "remove" : "add" } 
+        onClick={ handleClick }>
+          { inCart ? "Remove From Cart" : "Add to Cart" }</button>
     </li>
   );
 }
